b4ac9cced6b9f61bd6d10aa29725847ba640d2d5
David-Kirsch <davidjkirsch@gmail.com>
2020-08-28 15:12:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2
+
+let random = Math.random(2) * 100
+console.log(random)
+
+let mod = 13 % 9 ;
+
+let max = Math.max(0, 5, 20, 2);
